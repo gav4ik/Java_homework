f39b318b95aa91d4890c21866c7bd8fd4410b869
gav4ik <zhuky@yandex.ru>
2021-01-30 15:17:28 +0300

    comparison operators

--- src/main/java/ru/appline/homework1/Welcome.java
+++ src/main/java/ru/appline/homework1/Welcome.java
@@ -1,7 +1,17 @@
 package ru.appline.homework1;
 
+import java.util.Scanner;
+
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int a=1;
+        int b=2;
+        int c =3;
+        System.out.println(b += a *= 2);
+        a = b = c;
+        System.out.println(a);
+        int d = c-- % ++a;
+
     }
-}
+    }
